95d276ab9842d964a54a083c6fc09990a3f7d842
Bogdan <soaqa57@gmail.com>
2020-09-09 22:44:11 +0300

    configure postres

--- visitka/settings.py
+++ visitka/settings.py
@@ -78,8 +78,12 @@ WSGI_APPLICATION = 'visitka.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'djangodb',
+        'USER': 'djangouser',
+        'PASSWORD': 'R7iHn9wD',
+        'HOST': 'localhost',
+        'PORT': '5432'
     }
 }
 
